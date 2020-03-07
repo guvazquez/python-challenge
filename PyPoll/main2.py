e2783cf8054bfde6fe73d4b016913e58626a963e
guvazquez <61570921+guvazquez@users.noreply.github.com>
2020-03-07 12:27:03 -0600

    Update main2.py

--- PyPoll/main2.py
+++ PyPoll/main2.py
@@ -1,44 +1,9 @@
 import os
 import csv
 
-#f=open("Output//output.txt","w")
-csvpath = "Resources//budget_data.csv"
-
-with open(csvpath) as csvfile:
-    csvreader=csv.reader(csvfile,delimiter=',')
-    csvheader= next(csvfile)
-
-    Months=0
-    Total_amount = 0
-    Increase = []
-    Fechas = []
-    Valor=0
-    for row in csvreader:
-        Total_amount= Total_amount + int(row[1])
-        Increase.append(-int(Valor)+int(row[1]))
-        Fechas.append(row[0])
-        Valor=row[1]
-        Months+=1
-Tabla=dict(zip(Increase,Fechas))
-Mayor=max(Tabla.keys())
-Menor=min(Tabla.keys())
-Increase.pop(0)
-
-Suma=0
-for i in Increase:
-    Suma=Suma+i
-
-print("Financial Analisys ")
-print("------------------------------------- ")
-print("Total Months:  " + str(Months))
-print("Total:  $" + str(Total_amount))
-print("Average Change:  $" + str(round(Suma/(Months-1),2)))
-print("Greatest Increase in Profits: " + Tabla.get(Mayor) + " $ " + str(Mayor))
-print("Greatest Decrease in Profits: " + Tabla.get(Menor) + " $ " + str(Menor))
-
+f=open("Output//output.txt","w")
 
 csvpath1 = "Resources//election_data.csv"
-
 
 with open(csvpath1) as csvfile:
     csvreader=csv.reader(csvfile,delimiter=',')
@@ -50,7 +15,14 @@ with open(csvpath1) as csvfile:
         Votes+=1
         Candidates.append(row[2])
 
-print("Election Results ")
+#To file
+print("\n\n\nElection Results ",file=f)
+print("------------------------------------- ",file=f)
+print("Total Votes:  " + str(Votes),file=f)
+print("------------------------------------- ",file=f)
+
+#To terminal
+print("\n\n\nElection Results ")
 print("------------------------------------- ")
 print("Total Votes:  " + str(Votes))
 print("------------------------------------- ")
@@ -65,11 +37,19 @@ Winner=0
 for i in unicos:
     Votos_count= Candidates.count(unicos[a])
     Votos_percentage = round(((Votos_count/Votes)*100),0)
+    print(unicos[a] + ": " + str(Votos_percentage)+ "%  ("+ str(Votos_count)+")",file=f)
     print(unicos[a] + ": " + str(Votos_percentage)+ "%  ("+ str(Votos_count)+")")
     if Votos_count > Winner:
         Winner=Votos_count
         Winner_Candidate=unicos[a]
     a=a+1
 
+#To file
+print("------------------------------------- ",file=f)
+print("Winner:  " + Winner_Candidate ,file=f)
+
+#To Terminal
 print("------------------------------------- ")
 print("Winner:  " + Winner_Candidate )
+
+f.close()
